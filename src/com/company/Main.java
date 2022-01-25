f0b76cc3bf512c6278da2f8c6687e9937c30ec16
reza ghanbari <rezaqanbari1925@gmail.com>
2022-01-25 09:24:36 +0330

    Add print of number of expressions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,16 @@ package com.company;
 import components.ExpressionGenerator;
 import components.Solver;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
         ExpressionGenerator generator = new ExpressionGenerator();
         Solver solver = new Solver();
-        System.out.println(solver.solveExpressions(generator.generateExpressions(inputHandler.getExpressionLength()), inputHandler.getFinalResult()));
+        ArrayList<String> expressions = solver.solveExpressions(generator.generateExpressions(inputHandler.getExpressionLength()), inputHandler.getFinalResult());
+        System.out.println(expressions.size() + " expressions found");
+        System.out.println(expressions);
     }
 }
